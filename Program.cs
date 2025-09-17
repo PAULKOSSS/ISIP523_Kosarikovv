d5e30d3e81b271dee5c68ec751c426de3cf14359
bobrob <pavelkosarikov@gmail.com>
2025-09-17 11:49:07 +0300

    Написал конвертор валют

--- Program.cs
+++ Program.cs
@@ -25,6 +25,84 @@ class Pr1
         Console.WriteLine(average.ToString(), max.ToString(), min.ToString(), sum.ToString());
     }
 
+    static void Sort(int[] prises, string[] products,int quan_oper)
+    {
+        for (int i = 0; i < quan_oper; i++)
+            for (int j = 0; j < prises[i] - 1; j++)
+            {
+                if (prises[i] >  prises[j])
+                {
+                    int tempi = prises[i];
+                    prises[i] = prises[j];
+                    prises[j] = tempi;
+                    string temps = products[i];
+                    products[i] = products[j];
+                    products[j] = temps;
+                }
+            }
+    }
+
+    void currencyConverter(int[] prises, string[] products, int quan_oper)
+    {
+        Console.WriteLine("Выберите в какую валюту хотите перевести: ");
+        Console.WriteLine("1. Доллар");
+        Console.WriteLine("2. Евро");
+        Console.WriteLine("3. Квача");
+        Console.WriteLine("4. Своя валюта");
+
+        decimal[] costcopy = new decimal[quan_oper];
+        for (int i = 0; i < quan_oper; i++)
+        {
+            costcopy[i] = prises[i];
+        }
+
+        int n = Convert.ToInt32(Console.ReadLine());
+        //string choice = "";
+        decimal usdRate = 83.0m;
+        decimal eurRate = 97.0m;
+        decimal kvachRate = 0.28m;
+        decimal perRate = 0;
+
+        if (n == 1)
+        {
+            //choice = "Доллар";
+            for (int i = 0; i < quan_oper; i++)
+            {
+
+                costcopy[i] = (decimal)(costcopy[i] / usdRate);
+            }
+        }
+        if (n == 2)
+        {
+            //choice = "Евро";
+            for (int i = 0; i < quan_oper; i++)
+            {
+                costcopy[i] = (decimal)(costcopy[i] / eurRate);
+            }
+        }
+        if (n == 3)
+        {
+            //choice = "Квача";
+            for (int i = 0; i < quan_oper; i++)
+            {
+                costcopy[i] = (decimal)(costcopy[i] / kvachRate);
+            }
+        }
+        if (n == 4)
+        {
+            Console.WriteLine("Введите курс: ");
+            perRate = Convert.ToDecimal(Console.ReadLine());
+            for (int i = 0; i < quan_oper; i++)
+            {
+                costcopy[i] = (decimal)(costcopy[i] / perRate);
+            }
+        }
+        for (int i = 0; i < quan_oper; i++)
+        {
+            Console.WriteLine($"{products[i]} - {costcopy[i]}");
+        }
+    }
+
     static void Main(String[] args)
     {
         Console.Write("Введите кол-во операций: ");
@@ -63,6 +141,8 @@ class Pr1
                     Stat(prises, quan_oper);
                     break;
                 case 3:
+                    Sort(prises, products, quan_oper);
+                    Output(prises, products, quan_oper);
                     break;
                 case 4:
                     break;
