d2ebc42cd95cd6f08c7c5379bc448cd4a06bb6a7
234819 <pavelkosarikov@gmail.com>
2025-09-22 17:10:37 +0300

    написал первоначальную структуру программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Product
+{
+    public static int Count = 1;
+    public static string[] categories = new string[3] { "продукты", "косметика", "хозтовары" };
+    public int Id;
+    public string Name;
+    public int Price;
+    public int Quantity;
+    public bool isAvaliable;
+    public string Category;
+
+
+}
+
+class Programm
+{
+    static void Add(List<Product> products)
+    {
+        products.Add(new Product());
+    }
+
+    static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>();
+    }
+}
