fda329a65193393b34a8814dfa67bb20522e5027
Павел Косариков <pavelkosarikov@gmail.com>
2025-10-28 19:27:23 +0300

    Прописал методы добавления и удаления в классе

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 
 class Pr4 
 {
@@ -35,12 +36,71 @@ class Pr4
 
         static void AddBook(List<Book> Books)
         {
+            string name, author;
+            int price;
+            DateOnly d_of_pub;
+            var_genre genre;
 
+            Console.Write("Введите название: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите автора: ");
+            author = Console.ReadLine();
+
+            Console.Write("Введите цену: ");
+            if (!int.TryParse(Console.ReadLine(), out price)) {
+                Console.WriteLine("Неверный ввод");
+            }
+
+            Console.Write("Введите дату: ");
+            if (!DateOnly.TryParse(Console.ReadLine(), out d_of_pub))
+            {
+                Console.WriteLine("Неверный ввод");
+            }
+
+            Console.WriteLine("1.Детектив");
+            Console.WriteLine("2.Приключения");
+            Console.WriteLine("3.Роман");
+            Console.Write("Введите жанр: ");
+            if (!int.TryParse(Console.ReadLine(), out int choice_genre))
+            {
+                Console.WriteLine("Неверный ввод");
+            }
+            genre = (var_genre)(choice_genre - 1);
+
+            Book added_book = new Book(name, author, price, d_of_pub, genre);
+            var context = new ValidationContext(added_book);
+            var results = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(added_book, context, results, true))
+            {
+                Console.WriteLine("Не удалось создать объект User");
+                foreach (var error in results)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+                Console.WriteLine();
+            }
+            else
+                Console.WriteLine($"Объект User успешно создан. Name: {added_book.Name}\n");
         }
 
         static void DeleteBook(List<Book> Books)
         {
+            foreach (var book in Books) { 
+                Console.WriteLine($"{book.Id} - {book.Name}");
+                
 
+            }
+
+            Console.Write("Выберите книгу, которую хотите удалить, введя ее Id: ");
+            if (!int.TryParse(Console.ReadLine(), out int choice_id))
+            {
+                Console.WriteLine("Неверный ввод");
+            }
+
+            var book_to_delete = Books.FirstOrDefault(b => b.Id == choice_id);
+            Books.Remove(book_to_delete);
+            Console.WriteLine($"Книга {book_to_delete.Name} успешно удалена");
         }
 
         static void SearchBook(List<Book> Books)
