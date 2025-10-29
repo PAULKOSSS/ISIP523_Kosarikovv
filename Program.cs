24c1445f02ad7e12d93104743f3eba29745a1711
234819 <pavelkosarikov@gmail.com>
2025-10-29 13:50:55 +0300

    Создал класс игрока и абстрактный класс для всех персонажей

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Pr6
+{
+    class Programm
+    {
+        public class Character
+        {
+            public int HP;
+            public int Attack_value;
+            public int Defense_value;
+
+            public Character(int hp, int attack, int defense)
+            {
+                HP = hp;
+                Attack_value = attack;
+                Defense_value = defense;
+            }
+        }
+
+        class Weapon
+        {
+            public string WeaponName;
+            public float AttackFactor;
+        }
+
+        class Armor
+        {
+            public string ArmorName;
+            public float DefenseFactor;
+        }
+
+        class Player : Character 
+        {
+            public Weapon Player_weapon;
+            public Armor Player_armor;
+
+            public Player(int hp, int attack, int defense, Weapon weapon, Armor armor) 
+                :base (hp, attack, defense)
+            {
+                Player_weapon = weapon;
+                Player_armor = armor;
+            }
+
+            public void Attack()
+            {
+
+            }
+
+            public void Defense()
+            {
+
+            }
+
+            public void PickUpItem()
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
