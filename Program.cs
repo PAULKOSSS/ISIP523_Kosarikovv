06c10998606a4812a77679b31ba40697adf8330a
Павел Косариков <pavelkosarikov@gmail.com>
2025-11-04 14:26:56 +0300

    создал базовый абстрактный класс Person

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Pr5
+{
+    class Programm
+    {
+        class Person
+        {
+            public string FIO { get; private set; }
+            public DateOnly DateOfBirth { get; private set; }
+            public string PhoneNumber { get; private set; }
+            public string Email { get; private set; }
+
+            public Person(string fio, DateOnly dofb, string number, string email) 
+            {
+                FIO = fio;
+                DateOfBirth = dofb;
+                PhoneNumber = number;
+                Email = email;
+            }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine("Вся информация о человеке:");
+                Console.WriteLine($"ФИО - {FIO}");
+                Console.WriteLine($"Дата рождения - {DateOfBirth}");
+                Console.WriteLine($"Номер телефона - {PhoneNumber}");
+                Console.WriteLine($"Электронная почта - {Email}");
+                Console.WriteLine("");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
