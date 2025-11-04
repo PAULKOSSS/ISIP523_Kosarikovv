70a5a5aa2294de8ed356d516707c040991f088fd
Павел Косариков <pavelkosarikov@gmail.com>
2025-11-04 15:41:46 +0300

    Добавил функцию создания студентов и преподавателей

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Pr5
+﻿using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Pr5
 {
     class Programm
     {
+        List<Student> student;
+        List<Professor> professors;
+        List<Course> courses;
+
         class Person
         {
             public string FIO { get; private set; }
@@ -26,25 +33,130 @@
                 Console.WriteLine($"Электронная почта - {Email}");
                 Console.WriteLine("");
             }
+
+            public (string, DateOnly, string, string) Create()
+            {
+                string fio, number, email;
+                DateOnly dofb;
+
+                Console.WriteLine("Добавление человека...");
+                while (true)
+                {
+                    Console.Write("Введите ваше имя: ");
+                    fio = Console.ReadLine()?.Trim();
+
+                    if (!string.IsNullOrWhiteSpace(fio))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Ошибка! Имя не может быть пустым. Попробуйте снова.");
+                }
+
+                while (true)
+                {
+                    Console.Write("Введите ваш номер телефона: ");
+                    number = Console.ReadLine()?.Trim();
+
+                    if (!string.IsNullOrWhiteSpace(number))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Ошибка! Номер не может быть пустым. Попробуйте снова.");
+                }
+
+                while (true)
+                {
+                    Console.Write("Введите ваш адрес электронной почты: ");
+                    email = Console.ReadLine()?.Trim();
+
+                    if (!string.IsNullOrWhiteSpace(email))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Ошибка! Адрес электронной почты не может быть пустым. Попробуйте снова.");
+                }
+
+                while (true)
+                {
+                    Console.Write("Введите дату рождения (дд.мм.гггг): ");
+                    string input = Console.ReadLine()?.Trim();
+
+                    if (DateOnly.TryParseExact(input, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out dofb))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Ошибка! Неверный формат даты. Используйте дд.мм.гггг");
+                }
+
+                return (fio, dofb, number, email);
+            }
+
+            //public abstract void Add();
         }
+
 
         class Student : Person 
         {
-            public Student(string fio, DateOnly dofb, string number, string email) : base(fio, dofb, number, email)
+            public List<Course> courses_of_student;
+
+            public Student(string fio, DateOnly dofb, string number, string email) : base(fio, dofb, number, email) { }
+
+            public void Add(List<Student> students)
             {
+                var (fio, dofb, number, email) = Create();
+                Student student = new(fio, dofb, number, email);
+                students.Add(student);
+            }
+
+            public void EnrollmentOnCourse()
+            {
+
+            }
+
+            public void ShowAllCourses(List<Course> courses)
+            {
+
             }
         }
 
         class Professor : Person
         {
-            public Professor(string fio, DateOnly dofb, string number, string email) : base(fio, dofb, number, email)
+            public Professor(string fio, DateOnly dofb, string number, string email) : base(fio, dofb, number, email) { }
+
+            public void Add(List<Professor> professors)
             {
+                var (fio, dofb, number, email) = Create();
+                Professor professor = new(fio, dofb, number, email);
+                professors.Add(professor);
             }
         }
 
         class Course
         {
+            public string NameOfCourse { get; private set; }
+            public Professor ProfessorOfCourse { get; private set; }
+            public List<Student> StudentsOnCourse { get; set; }
 
+            public Course(string nameOfCourse, Professor professorOfCourse)
+            {
+                NameOfCourse = nameOfCourse;
+                ProfessorOfCourse = professorOfCourse;
+            }
+
+            public void CreateNewCourse(List<Course> courses)
+            {
+                
+            }
+
+            public void ShowAllInfo()
+            {
+
+            }
+
+            public void ShowAllStudentsOnCourse(List<Student> StudentsOnCourse)
+            {
+
+            }
         }
 
         static void PrintAllInfo()
